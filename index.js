b87ecf827f9fa74da3b86ab21791647e06eaa6cb
HieltDerg <147934660+HieltDerg@users.noreply.github.com>
2023-10-14 17:29:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Index.js');
+let name = 'e?'; // String
+let nameFirst = 'e?'; // String
+let nameSecond = 'e?'; // String
+let nameThird = 'e?'; // String
+let age = 30; // Number
+let isApproved = false // Boolean
+let firstName
+console.log(name);
